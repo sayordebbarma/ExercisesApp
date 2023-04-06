9bbdbfa22e4e8d6026d258640fbc4c456606559d
sayordebbarma <sayor.debbarma2021@vitbhopal.ac.in>
2023-04-06 20:07:26 +0530

    added some components

---
+++ src/components/HeroBanner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+
+const HeroBanner = () => {
+  return (
+    <Box>
+        HeroBanner
+    </Box>
+  )
+}
+
+export default HeroBanner
